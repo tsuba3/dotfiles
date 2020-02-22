136159da6b8c48d658b86aa2726c3e97a22d655e
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-02-22 20:58:46 +0900

    [zsh] Rename env/MacBookPro-2017.zsh

---
+++ zsh/env/MacBookPro-2017.zsh
@@ -0,0 +1,41 @@
+#
+# path
+#
+
+export PDIR=~/Documents/Projects
+export PATH=~/.scala-2.12.6/bin:$PATH
+
+export PATH=/usr/local/Cellar/git/2.14.1/bin:$PATH
+
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+export GOPATH=~/.golang
+export PATH=$PATH:$GOPATH/bin
+
+export QT_DIR=~/.Qt
+export CGO_CXXFLAGS_ALLOW=".*" 
+export CGO_LDFLAGS_ALLOW=".*" 
+export CGO_CFLAGS_ALLOW=".*" 
+export QT_HOMEBREW=true
+
+
+export PATH=~/.nodebrew/current/bin:$PATH
+export PATH=~/.gcloud/bin:$PATH
+
+# Use Java8
+# export JAVA_HOME=`/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home -v "1.8"`
+# PATH=${JAVA_HOME}/bin:${PATH}
+
+export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_SDK_ROOT=/Users/tsubasa/Library/Android/sdk
+export PATH=~/.android-sdk/bin:$PATH
+
+export PATH=~/.flutter_tool/bin:$PATH
+
+export IDF_PATH=/Users/tsubasa/.esp/esp-idf
+export PATH=/Users/tsubasa/.esp/xtensa-esp32-elf/bin:$PATH
+alias idf="python2 $IDF_PATH/tools/idf.py"
+
+export PATH="$PATH:$HOME/.cargo/bin"
+
+export Boost_INCLUDE_DIR=/usr/local/Cellar/boost/1.72.0/include
+
