29cdebd60730e063659663a79fc11b76fbb3021d
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-12-12 15:02:51 +0900

    Add zsh/env/MacBookPro15.zsh

---
+++ zsh/env/MacBookPro16.zsh
@@ -0,0 +1,32 @@
+#
+# path
+#
+
+export PDIR=~/Documents/Projects
+
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+export GOPATH=~/.golang
+export PATH=$PATH:$GOPATH/bin
+
+export PATH=~/.nodebrew/current/bin:$PATH
+export PATH=~/.gcloud/bin:$PATH
+
+export PATH="$PATH:$HOME/.cargo/bin"
+
+export Boost_INCLUDE_DIRS=/opt/boost/include
+export Boost_LIBRARY_DIRS=/opt/boost/lib
+
+export PATH="$PATH:$HOME/.pyenv/bin"
+export PATH="$PATH:$HOME/.cpdf/OSX-Intel"
+
+alias cpdf-help="open ~/.cpdf/cpdfmanual.pdf"
+
+export ATCODER_TEMPLATE="$HOME/Documents/Projects/atcoder/0templates"
+
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# TeX Live
+export MANPATH="$MANPATH:/usr/local/texlive/2020/texmf-dist.doc/man"
+export INFOPATH="$INFOPATH:/usr/local/texlive/2020/texmf-dist/doc/info"
+export PATH="$PATH:/usr/local/texlive/2020/bin/x86_64-darwin"
+
