ddd3f3b52586de53de01c8997f53145748634c3f
Tsubasa Munekata <tsubasa.afe@gmail.com>
2021-05-18 15:00:43 +0900

    direnv

--- zsh/env/MacBookPro16.zsh
+++ zsh/env/MacBookPro16.zsh
@@ -17,6 +17,7 @@ export Boost_INCLUDE_DIRS=/opt/boost/include
 export Boost_LIBRARY_DIRS=/opt/boost/lib
 
 export PATH="$PATH:$HOME/.pyenv/bin"
+export PATH="$PATH:$HOME/.nodenv/bin"
 export PATH="$PATH:$HOME/.cpdf/OSX-Intel"
 
 alias cpdf-help="open ~/.cpdf/cpdfmanual.pdf"
@@ -29,7 +30,24 @@ export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/b
 export PATH="$HOME/.bin:$PATH"
 
 # TeX Live
-export MANPATH="$MANPATH:/usr/local/texlive/2020/texmf-dist.doc/man"
-export INFOPATH="$INFOPATH:/usr/local/texlive/2020/texmf-dist/doc/info"
-export PATH="$PATH:/usr/local/texlive/2020/bin/x86_64-darwin"
+export MANPATH="$MANPATH:/usr/local/texlive/2021/texmf-dist.doc/man"
+export INFOPATH="$INFOPATH:/usr/local/texlive/2021/texmf-dist/doc/info"
+export PATH="$PATH:/usr/local/texlive/2021/bin/universal-darwin"
+
+
+wiki() {
+    PORT=2880
+    echo $1
+    if [ -z "$1" ]; then
+        URL="http://localhost:$PORT/mediawiki/index.php?title=Nakayama_Laboratory_Wiki"
+    else
+        URL=`echo $1 | sed -e s/192.168.1.4/localhost:$PORT/g`
+    fi
+
+    (sleep 1 && open "$URL") &
+    echo open "$URL"
+    ssh -N -L $PORT:192.168.1.4:80 mirai
+}
+
+export FW_CONFIG_FILE="$HOME/.atomate/config/FW_config.yaml"
 
